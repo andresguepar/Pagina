05d612c0e81c0d9791179e723efa946a03bfa8c8
Rumi Bunny <jquiceno2219@cue.edu.co>
2022-12-05 12:42:09 -0500

    commit

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,24 @@
-
 let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
   
 }
+
+function alertar(mixin){  
+}
+
+const form = document.getElementById("checkout");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Purchase completed',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      
+}
+)
--- js/script.js
+++ js/script.js
@@ -97,8 +97,6 @@ const paint = data => {
 }
 
 const addCart = e => {
-    //console.log(e.target);
-    //console.log(e.target.classList.contains('btn'))
     if(e.target.classList.contains('btn')){
          setCart(e.target.parentElement);
 
@@ -212,25 +210,3 @@ function security() {
     }
 	
 }
-
-/*var swiper = new Swiper(".product-slider", {
-    loop:true,
-    spaceBetween: 20,
-    autoplay: {
-        delay:7500,
-        disableOnInteraction: false,
-    },
-    centeredSlides: true,
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1020: {
-        slidesPerView: 3,
-
-      },
-    },
-  });*/
